2b9b83086a731810656a2e944f21ed9e8d5fbc9c
Jakub Czartek <kuba@linux.pl>
2019-11-13 17:24:22 +0100

    Zadanie Kolory

---
+++ 09112019/Zadania/ColorWPF/ColorWPF/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Color RectangleColor
+        {
+            get { return (rectangle.Fill as SolidColorBrush).Color; }
+            set { (rectangle.Fill as SolidColorBrush).Color = value; }
+        }
+
+        private bool _afterInitialized = false;
+        private bool AfterInitialized
+        {
+            get { return _afterInitialized; }
+            set { _afterInitialized = value; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Settings.ReadWindowState(this);
+            Color color = Settings.ReadColorSettings();
+
+            silderR.Value = color.R;
+            silderG.Value = color.G;
+            silderB.Value = color.B;
+            rectangle.Fill = new SolidColorBrush(color);
+            AfterInitialized = true;
+        }
+
+        private void SilderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (AfterInitialized == false) return;
+
+            Color color = Color.FromRgb(
+                (byte)silderR.Value,
+                (byte)silderG.Value,
+                (byte)silderB.Value);
+
+            RectangleColor = color;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Settings.SaveColorSettings(RectangleColor);
+            Settings.SaveWindowState(this);
+        }
+    }
+}
---
+++ 09112019/Zadania/ColorWPF/ColorWPF/Settings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColorWPF
+{
+    public static class Settings
+    {
+        public static Color ReadColorSettings()
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+
+            return new Color() { A = 255, R = settings.R, B = settings.B, G = settings.G };
+        }
+
+        public static void SaveColorSettings(Color color)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+
+            settings.R = color.R;
+            settings.G = color.G;
+            settings.B = color.B;
+            settings.Save();
+        }
+
+        public static void ReadWindowState(MainWindow window)
+        {
+            var settings = Properties.Settings.Default;
+            window.Left = settings.Left;
+            window.Top = settings.Top;
+            window.Width = settings.Width;
+            window.Height = settings.Height;
+        }
+
+        public static void SaveWindowState(MainWindow window)
+        {
+            var settings = Properties.Settings.Default;
+            settings.Left = window.Left;
+            settings.Top = window.Top;
+            settings.Width = window.Width;
+            settings.Height = window.Height;
+            settings.Save();
+        }
+    }
+}
