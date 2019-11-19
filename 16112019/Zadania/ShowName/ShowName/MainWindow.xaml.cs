16e6c7e3f7e2319d840d0a8d3e8fda936c7a5cb9
Jakub Czartek <kuba@linux.pl>
2019-11-19 15:39:19 +0100

    Zadanie: Pokaż imię

---
+++ 16112019/Zadania/ShowName/ShowName/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+
+namespace ShowName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var letter in tbName.Text)
+                if (Char.IsDigit(letter) || Char.IsWhiteSpace(letter))
+                    throw new ArgumentException("Nie dozowlone litery");
+
+            if (!String.IsNullOrEmpty(tbName.Text))
+                MessageBox.Show(tbName.Text);
+        }
+    }
+}
