8f8d8e9795e343f9ed2d3b1bfeb5b652a8f4214e
Jakub Czartek <kuba@linux.pl>
2019-11-19 16:52:26 +0100

    Zadania: Dzielenie przez zero/Złapać wyjątek

---
+++ 16112019/Zadania/DzielenieTryCatch/DzielenieTryCatch/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DzielenieTryCatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbA.Text) || String.IsNullOrEmpty(tbB.Text))
+                return;
+
+            if (!String.IsNullOrEmpty(tblError.Text))
+                tblError.Text = "";
+
+            try
+            {
+                int a = int.Parse(tbA.Text);
+                int b = int.Parse(tbB.Text);
+                int result = a / b;
+
+                lResult.Content = result.ToString();
+            } catch (DivideByZeroException exn)
+            {
+                tblError.Text = exn.Message;
+            } catch (Exception exn)
+            {
+                tblError.Text = exn.Message;
+            }
+        }
+    }
+}
