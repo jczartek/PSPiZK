d0f865d4bd8c460a33452a863c583c80aaeafd93
Jakub Czartek <kuba@linux.pl>
2019-11-20 15:43:07 +0100

    Zadanie: Drag and Drop

---
+++ 16112019/Zadania/DragAndDrop/DragAndDrop/ListBoxExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DragAndDrop
+{
+    public static class ListBoxExtensions
+    {
+        public static ListBoxItem GetItemAt(ListBox listBox, Point position)
+        {
+            DependencyObject item = VisualTreeHelper.HitTest(listBox, position).VisualHit;
+
+            while (item != null && !(item is ListBoxItem))
+                item = VisualTreeHelper.GetParent(item);
+
+            return item as ListBoxItem;
+        }
+
+        public static int IndexFromPoint(ListBox listBox, Point position)
+        {
+            ListBoxItem item = GetItemAt(listBox, position);
+            return listBox.Items.IndexOf(item);
+        }
+    }
+}
---
+++ 16112019/Zadania/DragAndDrop/DragAndDrop/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DragAndDrop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ListBox source = sender as ListBox;
+            ListBoxItem item = ListBoxExtensions.GetItemAt(source, e.GetPosition(source));
+
+            if (item != null)
+            {
+                DataObject data = new DataObject();
+                data.SetData("Source", source);
+                data.SetData("MovedItem", item);
+                DragDrop.DoDragDrop(source, data, DragDropEffects.Copy | DragDropEffects.Move);
+            }
+        }
+
+        private void ListBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                e.Effects = DragDropEffects.Copy;
+            else
+                e.Effects = DragDropEffects.Move;
+            e.Handled = true;
+
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+            ListBox destination = sender as ListBox;
+            ListBox source = e.Data.GetData("Source") as ListBox;
+            ListBoxItem item = e.Data.GetData("MovedItem") as ListBoxItem;
+
+            if (!e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                source.Items.Remove(item);
+            else
+                item = new ListBoxItem { Content = item.Content };
+
+            int idx = ListBoxExtensions.IndexFromPoint(destination, e.GetPosition(destination));
+
+            if (idx < 0)
+                destination.Items.Add(item);
+            else
+                destination.Items.Insert(idx, item);
+
+        }
+    }
+}
