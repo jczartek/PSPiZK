85aeb07683a4bf780556b15032c562db7d4a2d4a
Jakub Czartek <kuba@linux.pl>
2019-11-27 14:29:12 +0100

    Zadanie: ImagePrinter

---
+++ 23112019/Zadania/ImagePrinter/ImagePrinter/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImagePrinter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog openFileDialog = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (openFileDialog == null)
+            {
+                openFileDialog = new OpenFileDialog();
+            }
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileName = new Uri(openFileDialog.FileName);
+                imageToPrint.Source = new BitmapImage(fileName);
+            }
+        }
+
+        private void printButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageToPrint.Source == null)
+                return;
+
+            Printing.PrintImage(imageToPrint);
+        }
+    }
+}
---
+++ 23112019/Zadania/ImagePrinter/ImagePrinter/Printing.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ImagePrinter
+{
+    public static class Printing
+    {
+        private static FlowDocument getFlowDocument(Image image, double widht, double height)
+        {
+            Image nImage = new Image() { Source = image.Source.Clone() };
+
+            if (nImage.Width > widht) nImage.Width = widht;
+
+            if (nImage.Height > height) nImage.Height = height;
+
+            return new FlowDocument(new BlockUIContainer(nImage));
+        }
+
+        public static void PrintImage(Image image)
+        {
+            PrintDialog printDialog = new PrintDialog();
+
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument flowDocument = getFlowDocument(image, printDialog.PrintableAreaWidth, printDialog.PrintableAreaHeight);
+                printDialog.PrintDocument((flowDocument as IDocumentPaginatorSource).DocumentPaginator, flowDocument.Name);
+            }
+        }
+    }
+}
