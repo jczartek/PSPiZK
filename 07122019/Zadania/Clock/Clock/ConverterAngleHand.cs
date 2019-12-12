567f9b286bd177c8c59cbcec51d15a730ec933a3
Jakub Czartek <kuba@linux.pl>
2019-12-12 15:14:51 +0100

    Zadanie: Clock

---
+++ 07122019/Zadania/Clock/Clock/ConverterAngleHand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Clock
+{
+    public enum Hands { Hour, Minute, Second}
+    class ConverterAngleHand : IValueConverter
+    {
+        public Hands Hands { get; set; } = Hands.Hour;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dt = (DateTime) value;
+            double valueAngle = 0;
+
+            switch(Hands)
+            {
+                case Hands.Hour:
+                    valueAngle = dt.Hour;
+                    if (valueAngle > 12) valueAngle -= 12;
+                    valueAngle += dt.Minute / 60.0;
+                    valueAngle /= 12.0;
+                    break;
+                case Hands.Minute:
+                    valueAngle = dt.Minute;
+                    valueAngle += dt.Second / 60.0;
+                    valueAngle /= 60.0;
+                    break;
+                case Hands.Second:
+                    valueAngle = dt.Second;
+                    valueAngle /= 60.0;
+                    break;
+            }
+            return valueAngle *= 360.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 07122019/Zadania/Clock/Clock/ViewModel/Clock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace Clock.ViewModel
+{
+    class Clock : INotifyPropertyChanged
+    {
+        private const int refreshViewModelMs = 250;
+        private readonly DateTime previousTime = DateTime.Now;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime CurrentTime
+        {
+            get { return DateTime.Now; }
+        }
+
+        private void OnPropertyChanged()
+        {
+            if (CurrentTime - previousTime < TimeSpan.FromSeconds(1) && CurrentTime == previousTime)
+                return;
+
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("CurrentTime"));
+        }
+
+        public Clock()
+        {
+            DispatcherTimer dTimer = new DispatcherTimer();
+
+            dTimer.Tick += (s, e) => { OnPropertyChanged(); };
+            dTimer.Interval = TimeSpan.FromMilliseconds(refreshViewModelMs);
+            dTimer.Start();
+        }
+    }
+}
