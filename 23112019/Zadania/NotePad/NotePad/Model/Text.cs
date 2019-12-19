8de29c20f177647c431ce5b6090fb1f1b925f2ed
Jakub Czartek <kuba@linux.pl>
2019-12-19 16:59:03 +0100

    Notepad: Model i Model Vidoku

---
+++ 23112019/Zadania/NotePad/NotePad/Model/Text.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePad.Model
+{
+    class Text : ICloneable
+    {
+        private StringBuilder sbuilder = new StringBuilder();
+
+        public string Content
+        {
+            get
+            {
+                return sbuilder.ToString();
+            }
+
+            set
+            {
+                sbuilder.Append(value);
+            }
+        }
+
+
+        public void Clear()
+        {
+            sbuilder.Clear();
+        }
+
+        public object Clone()
+        {
+            Text text = new Text
+            {
+                Content = ToString()
+            };
+            return text;
+        }
+
+        public override string ToString()
+        {
+            return sbuilder.ToString();
+        }
+    }
+}
---
+++ 23112019/Zadania/NotePad/NotePad/ViewModel/Editor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotePad.ViewModel
+{
+    using Model;
+    class Editor : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private Text text = new Text();
+        public string Content
+        {
+            get { return text.Content; }
+            set
+            {
+                text.Clear();
+                text.Content = value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+        public string PathFile { get; set; } = "[None]";
+    }
+}
